91025858a6e0a538fe30ed4005484fcd34e46473
Shkrebko-Misha <michael.shkrebko@gmail.com>
2026-03-17 22:50:33 +0300

    make task

--- tasks/shkrebko_m_calc_of_integral_rect/tests/performance/main.cpp
+++ tasks/shkrebko_m_calc_of_integral_rect/tests/performance/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "shkrebko_m_calc_of_integral_rect/common/include/common.hpp"
+#include "shkrebko_m_calc_of_integral_rect/omp/include/ops_omp.hpp"
 #include "shkrebko_m_calc_of_integral_rect/seq/include/ops_seq.hpp"
 #include "util/include/perf_test_util.hpp"
 
@@ -42,8 +43,9 @@ TEST_P(ShkrebkoMRunPerfTest, RunPerfModes) {
 }
 
 namespace {
-const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, ShkrebkoMCalcOfIntegralRectSEQ>(PPC_SETTINGS_shkrebko_m_calc_of_integral_rect);
+const auto kAllPerfTasks = std::tuple_cat(
+    ppc::util::MakeAllPerfTasks<InType, ShkrebkoMCalcOfIntegralRectSEQ>(PPC_SETTINGS_shkrebko_m_calc_of_integral_rect),
+    ppc::util::MakeAllPerfTasks<InType, ShkrebkoMCalcOfIntegralRectOMP>(PPC_SETTINGS_shkrebko_m_calc_of_integral_rect));
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
