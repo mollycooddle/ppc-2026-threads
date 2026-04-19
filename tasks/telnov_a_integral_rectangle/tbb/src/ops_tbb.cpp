44603d01bdbd1e5d357cd477d69d87376cb971ea
mollycooddle <toolya.telnoff@yandex.ru>
2026-04-19 15:13:52 +0300

    second_try

--- tasks/telnov_a_integral_rectangle/tbb/src/ops_tbb.cpp
+++ tasks/telnov_a_integral_rectangle/tbb/src/ops_tbb.cpp
@@ -34,26 +34,25 @@ bool TelnovAIntegralRectangleTBB::RunImpl() {
 
   const auto total_points = static_cast<int64_t>(std::pow(n, d));
 
-  const double result = oneapi::tbb::parallel_reduce(
-      oneapi::tbb::blocked_range<int64_t>(0, total_points), 0.0,
-      [n, d, a, h](const oneapi::tbb::blocked_range<int64_t> &range, double local_sum) {
-        for (int64_t idx = range.begin(); idx != range.end(); ++idx) {
-          int64_t tmp = idx;
-          double f_value = 0.0;
+  const double result =
+      oneapi::tbb::parallel_reduce(oneapi::tbb::blocked_range<int64_t>(0, total_points), 0.0,
+                                   [n, d, a, h](const oneapi::tbb::blocked_range<int64_t> &range, double local_sum) {
+    for (int64_t idx = range.begin(); idx != range.end(); ++idx) {
+      int64_t tmp = idx;
+      double f_value = 0.0;
 
-          for (int dim = 0; dim < d; ++dim) {
-            const int coord_index = static_cast<int>(tmp % n);
-            tmp /= n;
+      for (int dim = 0; dim < d; ++dim) {
+        const int coord_index = static_cast<int>(tmp % n);
+        tmp /= n;
 
-            const double x = a + ((static_cast<double>(coord_index) + 0.5) * h);
-            f_value += x;
-          }
+        const double x = a + ((static_cast<double>(coord_index) + 0.5) * h);
+        f_value += x;
+      }
 
-          local_sum += f_value;
-        }
-        return local_sum;
-      },
-      [](double lhs, double rhs) { return lhs + rhs; });
+      local_sum += f_value;
+    }
+    return local_sum;
+  }, [](double lhs, double rhs) { return lhs + rhs; });
 
   GetOutput() = result * std::pow(h, d);
   return true;
